78267dbc6fd85a86a511a19250b6751eedbee5b9
Mark Honeychurch <mark@honeychurch.org>
2020-07-09 00:08:44 +1200

    Making it pretty

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-var app = new Vue({
+const app = new Vue({
 	el: '#app',
 	data: {
 		filename: '',
-		workbook: [],
+		input: [],
+		multiplier: 1,
+		append: '',
+	},
+	computed: {
+		output() {
+			return this.input.map((row) => {
+				const output = {};
+				for (const [key, value] of Object.entries(row)) {
+					output[key] = (isNaN(value) ? value : value * this.multiplier) + this.append;
+				}
+				return output;
+			});
+		},
 	},
 	methods: {
 		upload(event) {
 			const reader = new FileReader();
 			reader.onload = (e) => {
-				const data = new Uint8Array(e.target.result);
-				const workbook = XLSX.read(data, {type: 'array'});
-				this.workbook = XLSX.utils.sheet_to_json(workbook.Sheets['Sheet1']);
-			}
-			// reader.readAsText(event.target.files[0]);
+				const workbook = XLSX.read(new Uint8Array(e.target.result), {type: 'array'});
+				this.input = XLSX.utils.sheet_to_json(workbook.Sheets['Sheet1']);
+			};
 			this.filename = event.target.files[0].name;
 			reader.readAsArrayBuffer(event.target.files[0]);
 		},
 		download() {
 			const workbook = XLSX.utils.book_new();
-			XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(this.workbook), 'Sheet1');
+			XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(this.output), 'Sheet1');
 			XLSX.writeFile(workbook, 'Test.xlsx');
-			/* const blob = new Blob(XLSX.utils.json_to_sheet(this.workbook), {type: 'application/vnd.ms-excel', name: 'Test.xslx'});
-			const link = document.createElement('a');
-			link.href = URL.createObjectURL(blob);
-			link.download = 'Test.xlsx';
-			link.click();
-			URL.revokeObjectURL(link.href); */
-		}
+		},
 	},
 });
